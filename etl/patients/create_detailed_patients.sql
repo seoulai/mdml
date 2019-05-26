97120cc6b1effec076047979254b9101d21206f8
James Park <laplacian.k@gmail.com>
2019-05-26 01:31:50 +0000

    Add columns in detailed_patients table

--- etl/patients/create_detailed_patients.sql
+++ etl/patients/create_detailed_patients.sql
@@ -1,11 +1,13 @@
 WITH patients AS
 (
+    -- https://mimic.physionet.org/tutorials/intro-to-mimic-iii/#5-patient-age-and-mortality
     SELECT st0.subject_id
            , st0.gender
            , ROUND( ( CAST( st1.first_admittime AS date) - CAST(st0.dob AS date)) / 365.242,2) AS age
-           , st0.dob
+           , st0.dob AS birth_date
            , st1.first_admittime
-           , st0.dod
+           , st1.last_admittime
+           , st0.dod AS death_date
            , CASE WHEN expire_flag = 0 THEN 'false'
                   ELSE 'true'
               END
@@ -16,6 +18,7 @@ WITH patients AS
     LEFT OUTER JOIN (
                      SELECT subject_id
                             , MIN( admittime) AS first_admittime
+                            , MAX( admittime) AS last_admittime
                        FROM admissions
                    GROUP BY subject_id
                      ) st1
