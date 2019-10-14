8083d8b5b2bb9625caadd841db2ed81fa4718cb7
James Park <laplacian.k@gmail.com>
2019-10-14 11:18:15 +0000

    Remove comments

--- etl/qsofa/daily/daily_qsofa.py
+++ etl/qsofa/daily/daily_qsofa.py
@@ -1,9 +1,9 @@
 from tb_tmp_daily_qsofa_log0 import TbTmpDailyqSOFALog0
 from tb_tmp_daily_qsofa_log1 import TbTmpDailyqSOFALog1
 from tb_tmp_daily_qsofa_log2 import TbTmpDailyqSOFALog2
-# from tb_tmp_daily_qsofa_log3 import TbTmpDailyqSOFALog3
-# from tb_tmp_daily_qsofa_log4 import TbTmpDailyqSOFALog4
-# from tb_tmp_daily_qsofa_log5 import TbTmpDailyqSOFALog5
+from tb_tmp_daily_qsofa_log3 import TbTmpDailyqSOFALog3
+from tb_tmp_daily_qsofa_log4 import TbTmpDailyqSOFALog4
+from tb_tmp_daily_qsofa_log5 import TbTmpDailyqSOFALog5
 
 
 class ETLDailyqSOFA():
@@ -12,12 +12,12 @@ class ETLDailyqSOFA():
     def run(
         self,
     ):
-        TbTmpDailyqSOFALog0().run()    # date
-        TbTmpDailyqSOFALog1().run()    # RR(respiratory rate)
-        TbTmpDailyqSOFALog2().run()    # BP(blood pressure)
+        # TbTmpDailyqSOFALog0().run()    # date
+        # TbTmpDailyqSOFALog1().run()    # RR(respiratory rate)
+        # TbTmpDailyqSOFALog2().run()    # BP(blood pressure)
         # TbTmpDailyqSOFALog3().run()    # GCS(Glasgow coma scale)
         # TbTmpDailyqSOFALog4().run()    # merge
-        # TbTmpDailyqSOFALog5().run()    # final
+        TbTmpDailyqSOFALog5().run()    # final
 
 
 if __name__ == "__main__":
